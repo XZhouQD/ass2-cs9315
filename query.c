87eee81a8ae146726465574f961a247e81762c80
Jiasi <dollylululu@gmail.com>
2019-08-07 18:39:19 +1000

    part2 query.c finished

--- query.c
+++ query.c
@@ -24,13 +24,13 @@ struct QueryRep {
 
 
 	// add struct attributes
-	int depth;
-	Bits start;   // start bucket value
-	Tuple qtuple;
+	int depth;    // linear hashing depth
+	Bits start;   // the begin value, pageId
+	Tuple qtuple; // string tuble
 
-	Count unnum;      //count how many unknown bits in the certain depth
-	Count ctuple;     // number of tuples scanned in curpage
-	Bits option;    // cur combination of unknown bits
+	Count unnum;      // the count umber of unkown bits in specific depth level
+	Count ctuple;     // the count number of tuples gotten in current page
+	Bits unbits;    // current unknow bits change level 
 };
 
 // take a query string (e.g. "1234,?,abc,?")
@@ -47,82 +47,76 @@ Query startQuery(Reln r, char *q)
 	// compute PageID of first page
 	//   using known bits and first "unknown" value
 	// set all values in QueryRep object
-	char buf[MAXBITS+1]; // for debug
-	new->rel = r;
-	Count nvals = nattrs(r);
-	char *vals[nvals];
-	tupleVals(q,vals);
 
-	int cmp[nvals];
-	Bits qknow = 0xFFFFFFFF;
-	Bits nknow = 0x00000000;
-	Bits mask = 0x00000000;
-	Bits hash[nvals];
-	for (int i = 0; i < nvals; i++)
-	{
-		if (vals[i] == NULL) fatal("Wrong number of attribute");
-		cmp[i] = strcmp(vals[i], "?");
-		if (!cmp[i]) {
-			hash[i] = 0x00000000;
-		} else hash[i] = hash_any((unsigned char *) vals[i], strlen(vals[i]));
-
-		bitsString(hash[i],buf);
-		printf("hash\"%s\" is %s\n",vals[i],buf);
-	}
-
-	ChVecItem *choiceVector = chvec(r);
-	//Not tested
-	for (int i = 0; i < MAXBITS; i++)
-	{
-		mask = 0x00000000;
-		if (!cmp[choiceVector[i].att])
-			nknow = setBit(nknow, i);
-		mask = setBit(mask, choiceVector[i].bit);
-		if ((hash[choiceVector[i].att] & mask) == 0)
-			qknow = unsetBit(qknow, i);
-	}
-
-	new->known = qknow;
-	new->unknown = nknow;
-	bitsString(qknow,buf);
-	printf("qknow is %s\n",buf);
-	mask = 0x00000000;
-	new->depth = depth(r);
-	for (int i = 0; i < depth(r); ++i)
-	{
-		mask = setBit(mask, i);
-	}
-
-	PageID id = qknow & mask;
-
-	if (id < splitp(r))
-	{
-		mask = setBit(mask, depth(r));
-		id = qknow & mask;
-		new->depth++;
-	}
-
-	int num = 0;
-	for (int i = 0; i < MAXBITS - 1; i++)
-	{
-		if (i >= new->depth)
-			break;
-		if (nknow & (0x00000001 << i))
-			num++;
-	}
-	new->curpage = id;
+	// initialize for some attributes
 	new->is_ovflow = 0;
 	new->curtup = 0;
-
-	new->unnum = num;
-	new->start = id;
-	new->qtuple = copyString(q);
-
-
+	new->rel = r;
+	new->depth = depth(r);
 	new->ctuple = 0;
+	new->unbits = 0;
 
+	// preparation
+	char buf[MAXBITS+1]; 
+	Count nvals = nattrs(r);
+	char *attr[nvals];
+	tupleVals(q, attr);
 
-	new->option = 0x00000000;
+	int cmp[nvals];
+	// create compare masks
+
+	Bits nknow = 0;
+	Bits comp = 0;
+	Bits hash[nvals];
+	Bits qknow = 0xFFFFFFFF;
+	for (int i = 0; i < nvals; i++) {
+		if (attr[i] == NULL) fatal("Wrong number of attribute");
+		cmp[i] = strcmp(attr[i], "?");
+		if (!cmp[i]) {
+			hash[i] = 0x00000000;
+		// hash
+		// equal
+		} else hash[i] = hash_any((unsigned char *) attr[i], strlen(attr[i]));
+		bitsString(hash[i],buf);
+	}
+	
+	// for known/unknown
+	ChVecItem *choiceVector = chvec(r);
+	for (int i = 0; i < MAXBITS; i++) {
+		// set
+		if (!cmp[choiceVector[i].att])
+			nknow = setBit(nknow, i);
+		// mask set
+		comp = 0;
+		comp = setBit(comp, choiceVector[i].bit);
+		if ((comp & hash[choiceVector[i].att]) == 0)
+			qknow = unsetBit(qknow, i);
+	}
+	bitsString(qknow,buf);
+	new->known = qknow;
+	new->unknown = nknow;
+	
+	comp = 0;
+	for (int i = 0; i < depth(r); i++) { comp = setBit(comp, i); }
+	Count rs = splitp(r);
+	// pageID set=> curr & start bucket value
+	PageID start = qknow & comp;
+	if (start < rs) {
+		new->depth++;
+		start = setBit(comp, depth(r)) & qknow;
+	}
+	new->curpage = start;
+	new->start = start;
+	// count unknow bits
+	int counts = 0;
+	for (int i = 0; i < MAXBITS - 1; i++) {
+		int move = 1 << i;
+		if (i >= new->depth) break; // out of range
+		if (move & nknow) counts++;
+	}
+	new->unnum = counts;
+	// compy query tuple string
+	new->qtuple = copyString(q);
 
 	return new;
 }
@@ -145,94 +139,81 @@ Tuple getNextTuple(Query q)
 	//    go to next page (try again)
 	// endif
 	Reln r = q->rel;
+	// loop condtion for valid check
+	while (TRUE) {
+		// get file
+		FILE *f;
+		if (q->is_ovflow != 1) f = dataFile(r);
+		else f = ovflowFile(r);
+		// get current page and the corresponding tuple number
+		Page current = getPage(f, q->curpage);
+		int overflow = pageOvflow(current);
+		Count n = pageNTuples(current);
 
-	while (TRUE)
-	{
-		PageID pid = q->curpage;
-		
-
-		FILE *file;
-		if (q->is_ovflow)
-		{
-			file = ovflowFile(r);
-		} else
-		{
-			file = dataFile(r);
-		}
-
-		Page p = getPage(file, pid);
 		//scan the cur page until there is no left tuples
 		//return if find match
-		while (q->ctuple < pageNTuples(p))
-		{
-			Tuple tmp = nextTuple(file,q->curpage,q->curtup);
+		while (q->ctuple < n) {
+			Tuple next;
 			q->ctuple++;
-			q->curtup = q->curtup + strlen(tmp) + 1;
-			//printf("all tuples : %s   %d\n", tmp, strlen(tmp));	//debug
-			if (tupleMatch(q->rel, q->qtuple, tmp))
-				return tmp;
-			free(tmp);
+			next = nextTuple(f, q->curpage, q->curtup);
+			int check = tupleMatch(q->rel, q->qtuple, next);
+			q->curtup = q->curtup + strlen(next) + 1;
+			if (check) return next;
+			else free(next);
 		}
 
-		//switch to next page or overflow
-
-		if (pageOvflow(p) != -1)
-		{
-			q->curpage = pageOvflow(p);
-			q->ctuple = 0;
-			q->curtup = 0;
-			q->is_ovflow = 1;
-			continue;
-		} else
-		{
+		// check overflow
+		// switch to next page
+		if (overflow == -1) {
+			// mask for set & compare
+			Bits setM = 0;
 			Bits uknow = q->unknown;
-			Bits op = q->option;
-			op++;
-			int i;
-			int count = q->unnum;
+			int unnums = q->unnum;
+			Bits uncount = q->unbits;
 
-			if (op >= (0x00000001 << count))
-			{
-				// including three cases:
-				// 1) op from 0x11111111 to 0x00000000, count == 8
-				// 2) op from 0x00000000 to 0x00000001, count == 0 (no unknown bits)
-				// 3) op from 0x0000000F to 0x00000010, count == 4 (normal case)
+			// eg: 1 << 1 => 0x00000010
+			int new_mask = 1 << unnums;
+
+			// check condition
+			// current unknow bits is equal or larger
+			uncount++;
+			if (new_mask <= uncount) {
 				break;
 			}
-			q->option = op;
-
-			int offset = 0;
-			Bits mask = 0;
-			for (i = 0; i < count; ++i)
-			{
-				while (!(uknow & (setBit(0, offset))))
-					offset++;
-
-				if (op & setBit(0, i))
-					mask = setBit(mask, offset);
-
-				offset++;
+			// bit set position
+			int pos = 0;
+			for (int i = 0; i < unnums; i++) {
+				while (!((setBit(0, pos) & uknow)))pos++;
+				if (setBit(0, i) & uncount) setM = setBit(setM, pos);
+				pos++;
 			}
-			PageID id = q->start | mask;
-
-			if(id > npages(r)-1)
+			// check  pages and mask
+			if((npages(r)-1) < (q->start | setM)  )
 				break;
-
-			q->curpage = id;
-			q->ctuple = 0;
-			q->curtup = 0;
+			// updates attributes
+			q->curpage = q->start | setM;
 			q->is_ovflow = 0;
+			q->curtup = 0;
+			q->ctuple = 0;
+			q->unbits = uncount;
 
+		} else { // not -1 
+			// updates attributes
+			Offset ov = pageOvflow(current);
+			q->is_ovflow = 1;
+			q->curtup = 0;
+			q->ctuple = 0;
+			q->curpage = ov;
+			continue;
 		}
 	}
-
+	// next get nothing
 	return NULL;
 }
 
 
 
 // clean up a QueryRep object and associated data
-
 void closeQuery(Query q)
 {
 	// TODO
